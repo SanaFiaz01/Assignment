3fd736373e5d858fb301ab704b68fdeda9b0557e
Sana Fiaz <fiazsana4@gmail.com>
2023-07-17 00:40:38 +0500

    second commit

---
+++ app/Controllers/CustomAuth.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use IonAuth\Libraries\IonAuth;
+
+class CustomAuth extends \IonAuth\Controllers\Auth
+{
+    protected $ionAuth;
+
+    public function __construct(){
+        $this->ionAuth = new IonAuth();
+        helper('form');
+        $this->validation = \Config\Services::validation();
+        $this->session = \Config\Services::session();
+
+    }
+    
+    public function index(){
+
+        if(!$this->ionAuth->loggedIn()){
+            return view('IonAuth\Views\auth\login');
+
+    }
+    else{
+        return redirect()->to('/dashboard');
+    }
+    }
+    public function login(){
+
+        $this->data['title'] = lang('Auth.login_heading');
+
+		// validate form input
+		$this->validation->setRule('identity', str_replace(':', '', lang('Auth.login_identity_label')), 'required');
+		$this->validation->setRule('password', str_replace(':', '', lang('Auth.login_password_label')), 'required');
+
+		if ($this->request->getPost() && $this->validation->withRequest($this->request)->run())
+		{
+			// check to see if the user is logging in
+			// check for "remember me"
+			$remember = (bool)$this->request->getVar('remember');
+
+			if ($this->ionAuth->login($this->request->getVar('identity'), $this->request->getVar('password'), $remember))
+			{
+				//if the login is successful
+				//redirect them back to the home page
+				$this->session->setFlashdata('message', $this->ionAuth->messages());
+				return redirect()->to('/')->withCookies();
+			}
+			else
+			{
+				// if the login was un-successful
+				// redirect them back to the login page
+				$this->session->setFlashdata('message', $this->ionAuth->errors($this->validationListTemplate));
+				// use redirects instead of loading views for compatibility with MY_Controller libraries
+				return redirect()->back()->withInput();
+			}
+		}
+		else
+		{
+			// the user is not logging in so display the login page
+			// set the flash data error message if there is one
+			$this->data['message'] = $this->validation->getErrors() ? $this->validation->listErrors($this->validationListTemplate) : $this->session->getFlashdata('message');
+
+			$this->data['identity'] = [
+				'name'  => 'identity',
+				'id'    => 'identity',
+				'type'  => 'text',
+				'value' => set_value('identity'),
+			];
+
+			$this->data['password'] = [
+				'name' => 'password',
+				'id'   => 'password',
+				'type' => 'password',
+			];
+
+			return $this->renderPage($this->viewsFolder . DIRECTORY_SEPARATOR . 'login', $this->data);
+		}
+}
+}
+
